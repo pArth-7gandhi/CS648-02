ae70fecc17f3f651488fddb09d37065933d05797
pArth-7gandhi <parthgandhi703@gmail.com>
2025-02-05 11:59:38 -0800

    Second Lab1

---
+++ M2-Lab1/js/01-functions-practice.js
@@ -0,0 +1,16 @@
+//STEP 1
+function halfNumber(n) {
+    window.console.log('Half of', n, 'is', n/2+'.');
+}
+//STEP 2
+function squareNumber(n) {
+    window.console.log('The result of squaring the number', n, 'is', n*n+'.');
+}
+//STEP 3
+function percentOf(m, n) {
+    window.console.log(m + ' is ', ((m*100)/n) + '% of ' + n + '.');
+}
+//STEP 4
+function findModulus(m, n) {
+    window.console.log(n%m + ' is the modulus of ' + m + ' and ' + n + '.');
+}
